9054530f3dbe8c0cbe1bd64f9ce94ff7a77936d8
augieafr <augieafriyansyah@gmail.com>
2021-10-01 20:48:56 +0700

    added PostResponse.kt
    this data model is used to hold posts data from remote

---
+++ app/src/main/java/com/augieafr/postsapp/data/source/remote/response/PostResponse.kt
@@ -0,0 +1,24 @@
+package com.augieafr.postsapp.data.source.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class PostResponse(
+
+	@field:SerializedName("PostResponse")
+	val postResponse: List<PostResponseItem>
+)
+
+data class PostResponseItem(
+
+	@field:SerializedName("id")
+	val id: Int,
+
+	@field:SerializedName("title")
+	val title: String,
+
+	@field:SerializedName("body")
+	val body: String,
+
+	@field:SerializedName("userId")
+	val userId: Int
+)
