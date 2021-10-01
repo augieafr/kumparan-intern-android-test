2c807b0b3b5008ba14d5cd2e2972dfb8b90e1ac7
augieafr <augieafriyansyah@gmail.com>
2021-10-01 21:31:25 +0700

    added ApiResponse.kt
    used as status(success, error, or empty) when fetch data from remote

---
+++ app/src/main/java/com/augieafr/postsapp/data/source/remote/network/ApiResponse.kt
@@ -0,0 +1,7 @@
+package com.augieafr.postsapp.data.source.remote.network
+
+sealed class ApiResponse<out R> {
+    data class Success<out T>(val data: T) : ApiResponse<T>()
+    data class Error(val errorMessage: String) : ApiResponse<Nothing>()
+    object Empty : ApiResponse<Nothing>()
+}
