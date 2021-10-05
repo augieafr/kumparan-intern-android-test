c5b0572956783378a9840f1acdc7f1ae001517ef
augieafr <augieafriyansyah@gmail.com>
2021-10-05 09:52:22 +0700

    feat: added the viewmodel, and recycler view to activity
    now this activity can show the list of post, the data is from viewmodel and showed in recycler view

--- app/src/main/java/com/augieafr/postsapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/augieafr/postsapp/ui/home/HomeActivity.kt
@@ -1,12 +1,68 @@
 package com.augieafr.postsapp.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
-import com.augieafr.postsapp.R
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.augieafr.postsapp.data.source.Resource
+import com.augieafr.postsapp.databinding.ActivityHomeBinding
+import com.augieafr.postsapp.ui.PostsAdapter
+import com.augieafr.postsapp.ui.ViewModelFactory
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var viewModel: HomeViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = PostsAdapter()
+
+        val factory = ViewModelFactory.getInstance(this)
+        viewModel = ViewModelProvider(
+            this,
+            factory
+        ).get(HomeViewModel::class.java)
+
+        viewModel.getAllPost.observe(this){ status ->
+            when(status){
+                is Resource.Error -> makeToast(this, "${status.message}")
+                is Resource.Loading -> viewModel.isLoading.postValue(true)
+                is Resource.Success -> {
+                    // call get user
+                    adapter.setData(status.data)
+                    adapter.onItemClick = {
+                        makeToast(this, "Click: ${it.userId}")
+                    }
+                    with(binding.rvPosts) {
+                        layoutManager = LinearLayoutManager(context)
+                        this.adapter = adapter
+                        setHasFixedSize(true)
+                    }
+                    viewModel.isLoading.postValue(false)
+                }
+            }
+        }
+        viewModel.isLoading.observe(this){ isLoading ->
+            loadingState(isLoading)
+        }
+    }
+
+    private fun loadingState(isLoading: Boolean){
+        if (isLoading) {
+            binding.progressBar.visibility = View.VISIBLE
+            binding.rvPosts.visibility = View.GONE
+        } else {
+            binding.progressBar.visibility = View.GONE
+            binding.rvPosts.visibility = View.VISIBLE
+        }
+    }
+    private fun makeToast(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 }
