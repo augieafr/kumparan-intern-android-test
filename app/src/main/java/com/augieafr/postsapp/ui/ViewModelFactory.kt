ece4160fc07796d630c7a2e141a3fee0cceaf924
augieafr <augieafriyansyah@gmail.com>
2021-10-05 09:33:58 +0700

    added ViewModelFactory.kt
    factory for all of the view model that require PostRepository

---
+++ app/src/main/java/com/augieafr/postsapp/ui/ViewModelFactory.kt
@@ -0,0 +1,35 @@
+package com.augieafr.postsapp.ui
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.augieafr.postsapp.data.source.PostRepository
+import com.augieafr.postsapp.injection.Injection
+import com.augieafr.postsapp.ui.home.HomeViewModel
+
+class ViewModelFactory private constructor(private val postRepository: PostRepository) :
+    ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        when {
+            modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
+                return HomeViewModel(postRepository) as T
+            }
+
+            else -> throw Throwable("Unknown ViewModel class: " + modelClass.name)
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var instance: ViewModelFactory? = null
+
+        fun getInstance(context: Context): ViewModelFactory =
+            instance ?: synchronized(this) {
+                instance ?: ViewModelFactory(Injection.provideRepository(context)).apply {
+                    instance = this
+                }
+            }
+    }
+}
